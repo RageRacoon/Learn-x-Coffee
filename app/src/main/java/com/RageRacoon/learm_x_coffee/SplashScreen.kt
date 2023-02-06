d74e4278ff32fb198f74dccbb03b65114ab17ada
Yorye <jorge.abrodes@gmail.com>
2023-02-06 20:27:42 +0100

    [2-0001]:Primeros pasos para una splash screen

---
+++ app/src/main/java/com/RageRacoon/learm_x_coffee/SplashScreen.kt
@@ -0,0 +1,40 @@
+package com.RageRacoon.learm_x_coffee
+
+import android.window.SplashScreen
+import androidx.compose.foundation.Image
+import androidx.compose.foundation.layout.Arrangement
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.tooling.preview.Preview
+
+@Composable
+fun SplashScreen() {
+    Splash()
+}
+
+@Composable
+fun Splash(){
+    Column(
+        modifier = Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally,
+        verticalArrangement = Arrangement.Center
+    ){
+        Image(
+            painter = painterResource(id = R.drawable.logo_test),
+            contentDescription = "Logo de la empresa"
+        )
+        Text(text = "Hola mundo")
+
+    }
+}
+
+@Preview(showBackground = true)
+@Composable
+fun ShowSplashScreenPreview(){
+    Splash()
+}
