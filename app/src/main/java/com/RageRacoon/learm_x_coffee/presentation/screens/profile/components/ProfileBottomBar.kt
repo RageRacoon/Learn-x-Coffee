90ccaef33e2ba82e35f8036d807f3108d78ed473
Yorye <jorge.abrodes@gmail.com>
2023-06-05 10:09:03 +0200

    [6-0005] Barra de navegacion registro

--- app/src/main/java/com/RageRacoon/learm_x_coffee/presentation/screens/profile/components/ProfileBottomBar.kt
+++ app/src/main/java/com/RageRacoon/learm_x_coffee/presentation/screens/profile/components/ProfileBottomBar.kt
@@ -1,11 +1,17 @@
 package com.RageRacoon.learm_x_coffee.presentation.screens.login.components
 
 import android.app.Activity
-import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.width
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.BottomAppBar
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.*
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.draw.scale
 import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.unit.dp
 import androidx.navigation.NavHostController
@@ -19,26 +25,62 @@ import androidx.hilt.navigation.compose.hiltViewModel
 @Composable
 fun ProfileBottomBar(navController: NavHostController,viewModel: ProfileViewModel = hiltViewModel()){
     val activity = LocalContext.current as? Activity
+    BottomAppBar(
+        contentPadding = PaddingValues(start = 5.dp, end = 5.dp),
+        backgroundColor = MaterialTheme.colors.primary,
+    ) {
+        Row(
+            modifier = Modifier
+                .fillMaxWidth()
+                .padding(vertical = 24.dp),
+            horizontalArrangement = Arrangement.SpaceEvenly,
+            verticalAlignment = Alignment.CenterVertically
+        ) {
+            Box(){
+                IconButton(
+                    modifier = Modifier.size(48.dp).scale(2.5f),
+                    onClick = { navController.navigate(AppScreen.MainScreen.rutaPantalla) }
+                ) {
+                    Icon(imageVector = Icons.Default.Home,
+                        contentDescription = "",
+                        tint = MaterialTheme.colors.background,
 
-    Row {
-        MyButton(
-            modifier = Modifier,
-            text = "EditarPerfil",
-            onClick = {
-                //A la hora de navegar a la siguiente pantalla, pasamos por la ruta, una String con todos los datos de usuario, en formato Json. en tiempo real
-                navController.navigate(
-                    AppScreen.EditProfileScreen.suminstrarUsuario(viewModel.userInfo.toJson()))
-            })
-        Spacer(modifier = Modifier.width(25.dp))
-        MyButton(
-            modifier = Modifier,
-            text = "LoginOut",
-            onClick = {
-                viewModel.logOut()
-                navController.navigate(route = AppScreen.LogInScreen.rutaPantalla){
-                    popUpTo(AppScreen.LogInScreen.rutaPantalla){inclusive = true}
+                        )
+
                 }
-            })
-    }
+            }
+            Box(){
+                IconButton(
+                    modifier = Modifier.size(48.dp).scale(2.5f),
+                    onClick = {
+                        viewModel.logOut()
+                        navController.navigate(route = AppScreen.LogInScreen.rutaPantalla){
+                            popUpTo(AppScreen.LogInScreen.rutaPantalla){inclusive = true}
+                        }}
+                ) {
+                    Icon(imageVector = Icons.Default.ExitToApp,
+                        contentDescription = "",
+                        tint = MaterialTheme.colors.background,
+                    )
 
+                }
+            }
+            Box(){
+                IconButton(
+                    modifier = Modifier.size(48.dp).scale(2.5f),
+                    onClick = {
+                        navController.navigate(
+                            AppScreen.EditProfileScreen.suminstrarUsuario(viewModel.userInfo.toJson()))
+                    }
+                ) {
+                    Icon(imageVector = Icons.Default.Edit,
+                        contentDescription = "",
+                        tint = MaterialTheme.colors.background,
+                    )
+
+                }
+            }
+
+        }
+    }
 }
